92b8f4b78c63a49f3fcb31e0d88700856acf1aee
deniyudi <deniyudi2@gmail.com>
2024-09-24 21:00:03 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
+        mkcert(),
         laravel({
             input: [
                 'resources/css/app.css',
